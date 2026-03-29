7ecbec00387a8f76ced551e274b3d7b6f53c0e41
aditya nath <adityanath5002@gmail.com>
2026-03-29 22:13:39 +0530

    (add) pretty-fomatter

--- test/test.cpp
+++ test/test.cpp
@@ -1,4 +1,4 @@
-#include"include/json.hpp"
+#include"../include/json.hpp"
 #include<iostream>
 int main(){
 //    std::string s = R"|(
@@ -9,41 +9,40 @@ int main(){
 // )|";
 
 
-//    std::string s = R"JSON(
-// {
-//   "name": "Aditya",
-//   "age": 21,
-//   "isStudent": true,
-//   "skills": ["C++", "Go", "Python"],
-//   "projects": [
-//     {
-//       "title": "JSON Parser",
-//       "completed": true,
-//       "lines": 1200
-//     },
-//     {
-//       "title": "CLI Todo",
-//       "completed": false,
-//       "lines": 300
-//     }
-//   ],
-//   "details": {
-//     "cgpa": 8.5,
-//     "active": true,
-//     "notes": "Loves systems programming\nand low-level work"
-//   },
-//   "emptyArray": [],
-//   "emptyObject": {},
-//   "nullField": null
-// }
-// )JSON";
-
-std::string s = R"JSON(
+   std::string s = R"JSON(
 {
-"con":man123
+  "name": "Aditya",
+  "age": 21,
+  "isStudent": true,
+  "skills": ["C++", "Go", "Python"],
+  "projects": [
+    {
+      "title": "JSON Parser",
+      "completed": true,
+      "lines": 1200
+    },
+    {
+      "title": "CLI Todo",
+      "completed": false,
+      "lines": 300
+    }
+  ],
+  "details": {
+    "cgpa": 8.5,
+    "active": true,
+    "notes": "Loves systems programming\nand low-level work"
+  },
+  "emptyArray": [],
+  "emptyObject": {},
+  "nullField": null
 }
 )JSON";
+
+
    json::JSON j =  json::Parse(s);
-   auto projects = j["projects"].as_array();
-   std::cout<<projects[0]["lines"].as_number()<<"\n";
+   // auto projects = j["projects"].as_array();
+   // std::cout<<projects[0]["lines"].as_number()<<"\n";
+
+   json::PrettyFormatter fmt(4);
+   std::cout<<fmt.format(j);
 }
